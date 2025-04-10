9572d885b495baa5a7892b1d7d3eec92f08205f1
The73756 <the73756the@gmail.com>
2025-04-10 14:06:53 +0900

    add history page and card adaptive vars

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,16 +1,57 @@
 import { Breadcrumbs } from '@/components/ui/breadcrumbs';
 import { DescBlock } from '@/components/desc-block';
+import { Card } from '@/components/card';
 
 const content = {
   title: 'История развития',
   desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
 };
 
+const sections = [
+  {
+    title: '1950-е',
+    img: '/banner.png',
+  },
+  {
+    title: '1960-е',
+    img: '/banner.png',
+  },
+  {
+    title: '1970-е',
+    img: '/banner.png',
+  },
+  {
+    title: '1980-е',
+    img: '/banner.png',
+  },
+  {
+    title: '1990-е',
+    img: '/banner.png',
+  },
+  {
+    title: '2000-е',
+    img: '/banner.png',
+  },
+  {
+    title: '2010-е',
+    img: '/banner.png',
+  },
+  {
+    title: '2020-е',
+    img: '/banner.png',
+  },
+];
+
 export default function History() {
   return (
     <div>
       <Breadcrumbs />
       <DescBlock title={content.title} desc={content.desc} />
+      <div className='mt-16 grid grid-cols-[repeat(auto-fit,minmax(140px,1fr))] gap-4 xs:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] md:mt-24 md:grid-cols-[repeat(auto-fit,minmax(290px,1fr))]'>
+        {sections.map((section) => (
+          <Card key={section.title} sm item={section} />
+        ))}
+      </div>
     </div>
   );
 }
--- components/card.tsx
+++ components/card.tsx
@@ -8,14 +8,16 @@ interface Props {
     desc?: string;
     img: string;
   };
-  size?: 'sm';
+  sm?: boolean;
   buttonText?: string;
 }
 
-export const Card = ({ item, size, buttonText }: Props) => {
+export const Card = ({ item, sm, buttonText }: Props) => {
   return (
     <div className='group relative md:overflow-hidden'>
-      <div className='h-full min-h-[400px] w-full transition-all duration-1000 ease-in-out group-hover:brightness-75 md:min-h-[332px] md:group-hover:translate-y-[-5%]'>
+      <div
+        className={`h-full ${sm ? 'min-h-[200px] xs:min-h-[250px]' : 'min-h-[400px]'} w-full transition-all duration-1000 ease-in-out group-hover:brightness-75 md:min-h-[332px] md:group-hover:translate-y-[-5%]`}
+      >
         <Image
           src={item.img}
           alt={item.title}
@@ -24,21 +26,24 @@ export const Card = ({ item, size, buttonText }: Props) => {
           className='h-full w-full object-cover'
         />
       </div>
-      <div className='absolute bottom-0 left-0 right-0 bg-beige-gradient p-5'>
-        <h4 className='line-clamp-2 text-center text-2xl font-medium text-red-accent'>
+      <div className='absolute bottom-0 left-0 right-0 bg-beige-gradient p-3 md:p-5'>
+        <h4 className='line-clamp-2 text-center text-xl font-medium text-red-accent md:text-2xl'>
           {item.title}
         </h4>
-        <div className='overflow-hidden transition-all duration-1000 ease-in-out md:max-h-0 md:group-hover:max-h-[332px]'>
-          <div className='flex flex-col gap-5 pt-5'>
+        <div
+          className={`${!item.desc ? 'max-md:hidden' : ''} overflow-hidden transition-all duration-1000 ease-in-out md:max-h-0 md:group-hover:max-h-[332px]`}
+        >
+          <div className='flex flex-col gap-5 pt-3 md:pt-5'>
             {item.desc && (
               <p className='line-clamp-5 text-red-accent'>{item.desc}</p>
             )}
-            <Button asChild>
+            <Button className='max-md:hidden' asChild>
               <Link href=''>{buttonText || 'Перейти'}</Link>
             </Button>
           </div>
         </div>
       </div>
+      <Link className='absolute inset-0 md:hidden' href='' />
     </div>
   );
 };
